ae0ccee4eab2ee54d800ccdee954a785c8468d74
amhine <nihadamhine@gmail.com>
2025-09-29 10:07:54 +0100

    structure de class wallet

--- src/com/wallet/metier/Wallet.java
+++ src/com/wallet/metier/Wallet.java
@@ -1,5 +1,51 @@
 package com.wallet.metier;
 
+  import java.util.Set;
+  import java.util.UUID;
+
 public abstract class Wallet {
+	
+	private UUID id;
+	private String adress;
+	private double solde;
+	private Set<Transaction> transactions;
+	
+	public Wallet(UUID id,String adress,Double solde, Set<Transaction> transactions) {
+		this.id= id != null ? id:UUID.randomUUID();
+		this.adress=adress;
+		this.solde=solde;
+		this.transactions=transactions;
+	}
+	
+	public UUID getId() {
+		return id;
+	}
+	
+	public String getAdress() {
+		return adress;
+	}
+	
+	public double getSolde() {
+		return solde;
+	}
+	
+	public Set<Transaction> getTransactions(){
+		return transactions;
+	}
+	
+	public void setId(UUID id) {
+		this.id=id;
+	}
+	
+	public void setAdress(String adress) {
+		this.adress=adress;
+	}
+	
+	public void setSolde(double solde) {
+		this.solde=solde;
+	}
+	public void setTransaction(Set<Transaction> transactions) {
+		this.transactions=transactions;
+	}
 
 }
